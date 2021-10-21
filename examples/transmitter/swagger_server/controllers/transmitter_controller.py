c0b923256e0edcb2e41317893ac223b2f3f54511
Josh Matz <jmatz@duosecurity.com>
2021-10-21 14:09:56 -0400

    Remove register method from wrong controller

--- examples/transmitter/swagger_server/controllers/transmitter_controller.py
+++ examples/transmitter/swagger_server/controllers/transmitter_controller.py
@@ -5,7 +5,6 @@ from swagger_server import business_logic
 from swagger_server.business_logic import StreamDoesNotExist
 from swagger_server.models import PollParameters
 
-from swagger_server.models import RegisterParameters
 
 JWKS_JSON = {
     "kty": "oct",
@@ -49,11 +48,3 @@ def jwks_json():
     :return: JSON Web Key Set for our Event Transmitter
     """
     return JWKS_JSON
-
-
-def register(body=None):
-    if connexion.request.is_json:
-        body = RegisterParameters.parse_obj(connexion.request.get_json())
-
-    token_json = business_logic.register(body.audience)
-    return token_json, 200
