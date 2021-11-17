9735b47ee34c9eee37dddc51f387f5debc64d4aa
CJ Linton <clinton@duosecurity.com>
2021-11-17 10:07:15 -0500

    Use Flask, create endpoint for triggering verification event

--- examples/receiver/receiver/__main__.py
+++ examples/receiver/receiver/__main__.py
@@ -5,6 +5,7 @@
 # that can be found in the LICENSE file.
 #
 
+import asyncio
 import argparse
 import sys
 import logging
@@ -14,148 +15,155 @@ import requests
 import threading
 import os
 import uuid
+import json
 from pathlib import Path
 from typing import Any
+from flask import Flask, request
 import jwt
 from jwcrypto.jwk import JWKSet
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from logging.config import dictConfig
 
 
-class TransmitterInfo:
-    """A class that holds the transmitter endpoint information"""
-
-    def __init__(self, info: dict[str, Any], unsafe: bool, audience: str, token: str):
-        # FIXME get issuer information
-        self.unsafe = unsafe
-        self.add_subject_endpoint = info['add_subject_endpoint']
-        self.configuration_endpoint = info['configuration_endpoint']
-        self.jwks_uri = info['jwks_uri']
-        self.remove_subject_endpoint = info['remove_subject_endpoint']
-        self.status_endpoint = info['status_endpoint']
-        self.verification_endpoint = info['verification_endpoint']
-        self.supported_methods = info['delivery_methods_supported']
-        self.jwks = JWKSet.from_json(requests.get(
-            self.jwks_uri, verify=not unsafe).text)
-        self.audience = audience
-        self.token = token
-
-    @property
-    def auth(self) -> dict[str, str]:
-        """Build the 'Authorization' line for the HTTP header"""
-        return {
-            'Authorization': f"Bearer {self.token}",
-        }
-
-
-def make_requests(info: TransmitterInfo, hostname: str):
-    """Make requests on the demo transmitter"""
-    # Pause to allow the http server that receives push commands to start
-    time.sleep(1)
-
-    # Set up push requests
-    config_response = requests.post(
-        url=info.configuration_endpoint,
-        verify=not info.unsafe,
-        json={
-            'delivery': {
-                'method': 'https://schemas.openid.net/secevent/risc/delivery-method/push',
-                'endpoint_url': f"http://{hostname}:5003"
-            },
-            'events_requested': [
-                'https://schemas.openid.net/secevent/risc/event-type/credential-compromise',
-            ]
-        },
-        headers=info.auth,
-    )
-    if not config_response.ok:
-        logging.error("Error configuring push receiver -- exiting")
-        config_response.raise_for_status()
-
-    add_subject_response = requests.post(
-        url=info.add_subject_endpoint,
-        verify=not info.unsafe,
-        json={
-            'subject': {
-                'format': 'email',
-                'email': '*'
-            }
-        },
-        headers=info.auth)
-
+async def wait_until_available(host, port):
     while True:
-        verification_response = requests.post(
-            url=info.verification_endpoint,
-            verify=not info.unsafe,
+        try:
+            _reader, writer = await asyncio.open_connection(host, port)
+            writer.close()
+            await writer.wait_closed()
+            break
+        except (socket.gaierror, ConnectionError, OSError, TypeError):
+            pass
+        await asyncio.sleep(1)
+
+
+class TransmitterClient:
+    """A class that holds information for interacting with the transmitter"""
+
+    def __init__(self, sse_config: dict[str, str], verify: bool, audience: str, auth: str):
+        # FIXME get issuer information
+        self.sse_config = sse_config
+        self.verify = verify
+        self.audience = audience
+        self.auth = auth
+
+    def configure_stream(self):
+        config_response = requests.post(
+            url=self.sse_config["configuration_endpoint"],
+            verify=self.verify,
             json={
-                'state': uuid.uuid4().hex,
+                'delivery': {
+                    'method': 'https://schemas.openid.net/secevent/risc/delivery-method/push',
+                    'endpoint_url': f"http://{socket.gethostname()}:5003/event"
+                },
+                'events_requested': [
+                    'https://schemas.openid.net/secevent/risc/event-type/credential-compromise',
+                ]
             },
-            headers=info.auth,
+            headers=self.auth,
         )
-        time.sleep(2)
+        if not config_response.ok:
+            logging.error("Error configuring push receiver -- exiting")
+            config_response.raise_for_status()
+
+        add_subject_response = requests.post(
+            url=self.sse_config["add_subject_endpoint"],
+            verify=self.verify,
+            json={
+                'subject': {
+                    'format': 'email',
+                    'email': '*'
+                }
+            },
+            headers=self.auth)
+
+    def request_verification(self):
+        """Make requests on the demo transmitter"""
+        verification_response = requests.post(
+            url=self.sse_config["verification_endpoint"],
+            verify=self.verify,
+            json={'state': uuid.uuid4().hex},
+            headers=self.auth,
+        )
+        verification_response.raise_for_status()
+        return verification_response
 
 
-def main():
-    time.sleep(5)  # Wait for transmitter to start
-    # FIXME do in docker compose rather than here
-
-    # Get this instance's hostname
-    hostname = socket.gethostname()
-    audience = f"http://{hostname}"
-
-    # Parse command line arguments
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-x", "--xmit", help="Set the hostname of the Shared Signals Transmitter")
-    parser.add_argument("--unsafe", action="store_true",
-                        help="connect to server ignoring TLS certificate")
+        "-x", "--xmit", default="transmitter",
+        help="Set the hostname of the Shared Signals Transmitter",
+    )
+    parser.add_argument(
+        "--unsafe", action="store_true",
+        default=os.environ.get("UNSAFE_TLS", True),
+        help="connect to server ignoring TLS certificate",
+    )
     args = parser.parse_args()
 
-    # If the name of the transmitter is not specified, assume we're in `docker compose`
-    # and the demo transmitter is named "transmitter"
-    args.xmit = args.xmit or "transmitter"
-
-    # Use env-vars or CLI for the "unsafe" flag
-    args.unsafe = args.unsafe or (os.environ["UNSAFE_TLS"] == "True")
+    # Wait for transmitter to be available
+    await wait_until_available(args.xmit, 443)
 
     # Register stream
+    audience = "http://example_receiver"
     reg = requests.post(f"https://{args.xmit}/register",
                         verify=not args.unsafe,
                         json={"audience": audience})
-    token = reg.json()["token"]
+    auth = {'Authorization': f"Bearer {reg.json()['token']}"}
 
-    # Get the transmitter's endpoints
-    r = requests.get(
+    # Get the transmitter's endpoints and setup jwks
+    sse_config_response = requests.get(
         f"https://{args.xmit}/.well-known/sse-configuration", verify=not args.unsafe)
+    sse_config = sse_config_response.json()
+    jwks_json = requests.get(sse_config['jwks_uri'], verify=not args.unsafe).text
+    jwks = JWKSet.from_json(jwks_json)
 
-    info = TransmitterInfo(r.json(), args.unsafe, audience, token)
+    client = TransmitterClient(sse_config, not args.unsafe, audience, auth)
+    client.configure_stream()
 
-    # Define a class that handles the "push requests"
-    class Handler(BaseHTTPRequestHandler):
-        def do_POST(self):
-            content_length = int(self.headers["Content-Length"])
-            body = self.rfile.read(content_length)
-            kid = jwt.get_unverified_header(body)["kid"]
-            jwk = info.jwks.get_key(kid)
-            key = jwt.PyJWK(jwk).key
-            decoded = jwt.decode(
-                jwt=body,
-                key=key,
-                algorithms=["ES256"],
-                audience=info.audience
-            )
-            logging.info(f"Got message: {decoded}")
-            self.send_response(202)
-            self.end_headers()
+    # Define a flask app that handles the push requests
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
 
-    server_address = ("0.0.0.0", 5003)
-    httpd = HTTPServer(server_address, Handler)
+    app = Flask(__name__)
 
-    t = threading.Thread(target=make_requests, args=[
-                         info, hostname])
-    t.start()
+    @app.route('/event', methods=['POST'])
+    def receive_event():
+        body = request.get_data()
+        kid = jwt.get_unverified_header(body)["kid"]
+        jwk = jwks.get_key(kid)
+        key = jwt.PyJWK(jwk).key
+        decoded = jwt.decode(
+            jwt=body,
+            key=key,
+            algorithms=["ES256"],
+            # issuer=info.issuer,
+            audience=audience,
+        )
+        app.logger.info(json.dumps(decoded, indent=2))
+        return "", 202
 
-    httpd.serve_forever()
+    @app.route('/request_verification')
+    def request_verification():
+        client.request_verification().raise_for_status()
+        return "Submitted request for verification event"
+
+    app.run("0.0.0.0", 5003)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
